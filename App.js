9b3995050783a35ed1a7206b24ff063813e11e60
Fabian <garciafabianemmanuel@gmail.com>
2024-04-22 13:40:05 -0300

    primera app react native

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { StyleSheet, Platform } from 'react-native';
+import { Appbar, PaperProvider, Card, Avatar, Text } from 'react-native-paper';
+
+
+
 
 export default function App() {
+
+  const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
+
+  const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Content title="React Native" />
+        <Appbar.Action icon="calendar" onPress={() => {showCalendar}} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      </Appbar.Header>
+
+      <Card>
+        {/* <Card.Cover source={{ uri: './assets/icon.png' }} /> */}
+        <Card.Content>
+          <Text>¿Que es React Native?</Text>
+          <Text>
+            React Native es un marco de desarrollo de aplicaciones móviles que permite a los desarrolladores crear aplicaciones nativas para dispositivos iOS y Android utilizando JavaScript y React, una biblioteca de JavaScript para construir interfaces de usuario. Fue desarrollado por Facebook y se lanzó por primera vez en 2015.
+          </Text>
+        </Card.Content>
+      </Card>
+   
+
+
+    </PaperProvider>
   );
 }
 
